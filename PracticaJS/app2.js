d0981ef23d2836ea25995c5ea4040eec99775c36
Felipe Gutierrez Madrid <felipe.madrid@yahoo.es>
2022-10-17 15:55:46 -0500

    se agreja archivo script.js con bandera para Menú

---
+++ PracticaJS/app2.js
@@ -0,0 +1,9 @@
+persona = "Felipe";
+saludo = "Desde el curso les mando un Saludo";
+
+
+document.getElementById("demo2.0").innerHTML = persona;
+document.getElementById("demo2.1").innerHTML = saludo;
+document.getElementById("demo2.2").innerHTML = persona + saludo;
+
+document.getElementById("demo3").innerHTML = typeof null +"<br>"
